7a9ae6e8e1b5bc091d9b3844b4de578697b21e95
Brody <75492924+Brody234@users.noreply.github.com>
2026-05-04 21:16:22 -0400

    Update input_object.py

--- leader/input_object.py
+++ leader/input_object.py
@@ -1,5 +1,6 @@
 from utilities.unique_id import unique_id
 
+# the object that input data maps to to give it a unique id so that outputs can be mapped to original clients
 class InputObject:
     def __init__(self, input_val, data):
         self.id = unique_id(data)
